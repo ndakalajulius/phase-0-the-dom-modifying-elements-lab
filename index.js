0b69022e788850b87624ce8b29944616f8b8718d
ndakalajulius <julius.ndakala@student.moringaschool>
2022-08-14 07:41:37 +0300

    completed asignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+const element = document.getElementById('main');
+
+element.remove();
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = "Julius Ndakala is the champion"
 // Write your code here!
